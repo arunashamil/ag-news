dba330c541f1d6ed8345eedd2c63eb01f420ad4a
shamil.a <shamil.a@phystech.edu>
2025-06-04 14:10:50 +0300

    Fixed onnx converter

--- ag_news/modules/compile_to_onnx.py
+++ ag_news/modules/compile_to_onnx.py
@@ -9,17 +9,29 @@ from ag_news.modules.trainer import TextClassifier
 
 def main(checkpoint_name: str) -> None:
     module = TextClassifier.load_from_checkpoint(f"{MODELS_PATH}/{checkpoint_name}")
-    print("Successfully loaded from checkpoints")
+    print("Successfully loaded from checkpoint")
     module.eval()
 
     vocab_size = module.hparams.vocab_size
-    input_array = torch.randint(0, vocab_size, (1, 128))
+    input_array = torch.randint(0, vocab_size, (1, 90))
 
-    module.to_onnx(ONNX_PATH, input_array, export_params=True)
+    module.to_onnx(
+        ONNX_PATH,
+        input_array,
+        export_params=True,
+        input_names=["PREPROCESSED_TEXT"],
+        output_names=["LOGITS"],
+        dynamic_axes={
+            "PREPROCESSED_TEXT": {0: "batch_size", 1: "seq_length"},
+            "LOGITS": {0: "batch_size"},
+        },
+    )
 
     print("Model compiled to ONNX")
 
-    ort_session = onnxruntime.InferenceSession(ONNX_PATH)
+    ort_session = onnxruntime.InferenceSession(
+        ONNX_PATH, providers=["CPUExecutionProvider"]
+    )
     input_name = ort_session.get_inputs()[0].name
     ort_inputs = {input_name: input_array.numpy().astype(np.int64)}
     ort_outs = ort_session.run(None, ort_inputs)
